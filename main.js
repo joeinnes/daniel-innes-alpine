a843b0a07f5410a6dcb23828ba6ab6295618ea0b
joeinnes <joe@joeinn.es>
2022-05-24 20:33:46 +0200

    5 files modified

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
 import '@picocss/pico';
 import './style.css';
 import Alpine from 'alpinejs';
+import { Directus } from '@directus/sdk';
+const directus = new Directus('https://api.traist.co.uk');
 window.Alpine = Alpine;
+Alpine.store('posts', {
+  setPosts(posts) {
+    this.data = posts;
+  },
+  data: [],
+});
+
+Alpine.store('authenticated', false);
+
 Alpine.start();
+
+async function init() {
+  try {
+    await directus.auth
+      .refresh()
+      .then((e) => {
+        Alpine.store('authenticated', true);
+      })
+      .catch(() => {});
+
+    const data = await directus.items('posts').readByQuery({
+      fields: ['*', 'files.directus_files_id'],
+    });
+    console.log(data);
+    Alpine.store('posts').setPosts(data.data);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+init();
