7b6434af1f666e46214932a054173efc113fadaa
joeinnes <joe@joeinn.es>
2022-05-24 21:52:55 +0200

    4 files modified

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import './style.css';
 import Alpine from 'alpinejs';
 import { Directus } from '@directus/sdk';
 const directus = new Directus('https://api.traist.co.uk');
+const defaultQuery = {
+  fields: ['*', 'files.directus_files_id'],
+  sort: ['-post_date'],
+  limit: 15,
+};
 window.Alpine = Alpine;
 Alpine.store('posts', {
   setPosts(posts) {
@@ -12,6 +17,31 @@ Alpine.store('posts', {
 });
 
 Alpine.store('authenticated', false);
+Alpine.store('page', 1);
+Alpine.store('paginate', async (param = 0) => {
+  let nextPage = 0;
+  if (!param) {
+    nextPage = Alpine.store('page') + 1;
+  } else {
+    nextPage = param;
+  }
+  Alpine.store('page', nextPage);
+
+  const data = await directus
+    .items('posts')
+    .readByQuery({ ...defaultQuery, page: Alpine.store('page') });
+
+  Alpine.store('posts').setPosts(data.data);
+});
+Alpine.store('login', async (email, password) => {
+  await directus.auth.login({ email, password });
+  Alpine.store('authenticated', true);
+  Alpine.store('authenticated', false);
+  const data = await directus
+    .items('posts')
+    .readByQuery({ ...defaultQuery, page: Alpine.store('page') });
+  Alpine.store('posts').setPosts(data.data);
+});
 
 Alpine.start();
 
@@ -24,10 +54,9 @@ async function init() {
       })
       .catch(() => {});
 
-    const data = await directus.items('posts').readByQuery({
-      fields: ['*', 'files.directus_files_id'],
-    });
-    console.log(data);
+    const data = await directus
+      .items('posts')
+      .readByQuery({ ...defaultQuery, page: Alpine.store('page') });
     Alpine.store('posts').setPosts(data.data);
   } catch (e) {
     console.error(e);
